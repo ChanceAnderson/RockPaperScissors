ff3045b0d7d6e83d5c0af9c0c68829524159a117
Chance Anderson <chance.cea.anderson@gmail.com>
2023-01-17 10:43:07 -0600

    Added rockPaperScissors function

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 function getComputerChoice(){
-    let choice = Math.floor(Math.random() * 3);
-    if (choice===0){
+    let num = Math.floor(Math.random() * 3);
+    if (num===0){
         return "Rock";
     } 
-    else if (choice === 1){
+    else if (num === 1){
         return "Paper";
     }
     else {
         return "Scissors";
+    }
+}
+
+function getPlayerChoice(){
+    let userChoice = prompt("Rock, paper, scissors?");
+    return userChoice;
+}
+
+function rockPaperScissors(){
+    let computerChoice = toLowerCase(getComputerChoice());
+    let playerChoice = toLowerCase(getPlayerChoice());
+    if (computerChoice === "rock" && playerChoice === "scissors"){
+        console.log("Computer wins.");
+    }
+    else if (computerChoice === "paper" && playerChoice === "scissors"){
+        console.log("You win.");
+    }
+    else if (computerChoice === "scissors" && playerChoice ==="paper"){
+        console.log("Computer wins.");
+    }
+    else if (computerChoice === "rock" && playerChoice==="paper"){
+        console.log("You win.");
+    }
+    else {
+        console.log("Draw");
     }
 }
