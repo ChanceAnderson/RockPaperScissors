06511d20dcb30973e8a8c6237f5cfb67efad38c8
Chance Anderson <chance.cea.anderson@gmail.com>
2023-01-27 12:20:50 -0600

    Added response logic

--- script.js
+++ script.js
@@ -2,22 +2,26 @@ const rock = document.querySelector("#rock");
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
 const response = document.querySelector('#response');
+let gameScore = 0;
+let gameResult = ""
 
 rock.addEventListener('click', () => {
     const choice = "rock";
     rockPaperScissors(choice);
+    response.textContent = `${gameScore} : ${gameResult}`;
 });
 
 paper.addEventListener('click', () => {
     const choice = "paper";
     rockPaperScissors(choice);
+    response.textContent = `${gameScore} : ${gameResult}`;
 });
 
 scissors.addEventListener('click', () => {
     const choice = "scissors";
     rockPaperScissors(choice);
+    response.textContent = `${gameScore} : ${gameResult}`;
 });
-
 
 
 
@@ -34,38 +38,46 @@ function getComputerChoice(){
     }
 }
 
-
-
-
-
 //function getPlayerChoice(){
     //let userChoice = prompt("Rock, paper, scissors?");
     //return userChoice;
 //}
 
-
-
-
-
-
 function rockPaperScissors(choice){
     let computerChoice = getComputerChoice().toLowerCase();
     if (computerChoice === "rock" && choice === "scissors"){
         console.log("Computer wins.");
+        gameScore -=1;
     }
     else if (computerChoice === "paper" && choice === "scissors"){
         console.log("You win.");
+        gameScore += 1;
     }
     else if (computerChoice === "scissors" && choice === "paper"){
         console.log("Computer wins.");
+        gameScore -=1;
     }
     else if (computerChoice === "rock" && choice === "paper"){
         console.log("You win.");
+        gameScore += 1;
     }
     else if (computerChoice=== "scissors" && choice === "rock"){
         console.log("You win.");
+        gameScore += 1;
     }
     else {
         console.log("Draw");
+        gameScore = gameScore;
     }
+
+    if (gameScore == 5){
+        gameScore = 0;
+        gameResult = "You won!"
+    } else if (gameScore == -5) {
+        gameScore = 0;
+        gameResult = "You lose!"
+    } else {
+        gameResult = gameResult;
+    }
+    
 }
